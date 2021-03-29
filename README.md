ad540faf835bc7e39c32aed09fe6719205fc9d4d
Mkona055 <mkona055@uottawa.ca>
2021-03-29 15:29:35 -0400

    LAB8

--- README.md
+++ README.md
@@ -1,29 +1,5 @@
 
+
 # CSI2532 - Lab9 - Jetons d'API
 
-
-## Exercice 1: Configurer PHPAPP
-
-``` git clone @github.com:professor-forward/phpapp.git ```
-
-
-
-## Exercice 2: Créer un client
-
-```sql
-CREATE TABLE clients(name varchar(100) PRIMARY KEY,
-                     token varchar(500) DEFAULT md5(random()::text),
-                     data jsonb);
-                     
-INSERT INTO clients(name,token)
-VALUES('Big Co.','d7d85f7eac7360d725b44d327445473e' ),
-('Small Co.','9f8983a8494c8a003e064374ffb77cb6');
-```
-
-
-
-### Exercice 3,4,5
-
-voir api.php
-
-Veuillez changer la ligne 9 avec vos identifiants 
+voir [lab9](lab9.pdf) et le code [ici](public/api.php)
--- public/api.php
+++ public/api.php
@@ -4,51 +4,59 @@ $all = getallheaders();
 $mutant = $all["X-Men"];
 $auth = explode(" ",$all["Authentication"]);
 list($type, $token) = $auth;
+$error =null;
+if($token == 'professorcharlesxavier'){
+	$reply = ["token" => $token,"type"=>$type];
+	echo json_encode($reply);
+	echo "\n";
+}else{
+	$dbconn = pg_connect("host=localhost port=5432 dbname=phpapp password =bonbon");
+	$result = pg_query($dbconn, "SELECT token FROM clients");
 
+	$count = pg_num_rows($result);
+	while ($row = pg_fetch_assoc($result)){
 
-$dbconn = pg_connect("host=localhost port=5432 dbname=phpapp password =yourpassword");
-$result = pg_query($dbconn, "SELECT token FROM clients");
+		$count--;
 
-$count = pg_num_rows($result);
-while ($row = pg_fetch_assoc($result)){
+		if ($token == $row['token']){
+			$reply = ["token" => $token,"type"=>$type];
+			echo json_encode($reply);
+			echo "\n";
+			break;
 
-	$count--;
-
-	if ($token == $row['token']){
-		$reply = ["token" => $token,"type"=>$type];
-		echo json_encode($reply);
-		break;
-
-	}else if($count == 0){
-		http_response_code(401);
-		$error = ["error"=>'Invalid Token'];
-		$headers = ["token"=>$token,"type"=>$type];
-		echo json_encode($error);
-		echo "\n";
-		echo json_encode($headers);
-		echo "\n";
-		break;
+		}else if($count == 0){
+			http_response_code(401);
+			$error = ["error"=>'Invalid Token'];
+			$headers = ["token"=>$token,"type"=>$type];
+			echo json_encode($error);
+			echo "\n";
+			echo json_encode($headers);
+			echo "\n";
+			break;
+		}
 	}
 }
 
-switch ($mutant){
-	case "Wolverine":
-	$name = "Logan";
-	$reply = ["mutant"=> $mutant, "name"=>$name];
-	echo json_encode($reply);
-	break;
+if($error == null){
+	switch ($mutant){
+		case "Wolverine":
+		$name = "Logan";
+		$reply = ["mutant"=> $mutant, "name"=>$name];
+		echo json_encode($reply);
+		break;
 
-	case null:
-	http_response_code(400);
-	$error = ["error"=>'Please provide an X-Men mutant and reveal their human name'];
-	$headers = ["headers"=>$all];
-	echo json_encode($error);
-	echo "\n";
-	echo json_encode($headers);
-	break;
+		case null:
+		http_response_code(400);
+		$error = ["error"=>'Please provide an X-Men mutant and reveal their human name'];
+		$headers = ["headers"=>$all];
+		echo json_encode($error);
+		echo "\n";
+		echo json_encode($headers);
+		break;
 
-	default:
-	$name = "Unknown";
-	$reply = ["mutant"=> $mutant, "name"=>$name];
-	echo json_encode($reply);
+		default:
+		$name = "Unknown";
+		$reply = ["mutant"=> $mutant, "name"=>$name];
+		echo json_encode($reply);
+	}
 }
